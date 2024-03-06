e851c00bf14ac0ee47288138fd3fceaa9486b234
Aquib Nawaz <aquibnawaz78692@gmail.com>
2024-03-06 09:50:32 +0530

    stream-4

--- app/stream.c
+++ app/stream.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "stream.h"
 #include "message.h"
 
@@ -66,6 +67,43 @@ struct StreamData* create_stream_data(char ** commands, int commandLen){
     return data;
 }
 
+char* validate_and_generate_key(char* new_id_str, char* top_id, int* error){
+    char* id_start;
+    long new_millis=-1,new_id=-1;
+    long curr_millis = strtol(top_id, &id_start, 10);
+    long curr_id = strtol(id_start+1, NULL, 10);
+    *error=1;
+    if(new_id_str[0]!='*') {
+        new_millis = strtol(new_id_str, &id_start, 10);
+    }
+    else{
+        new_millis = curr_millis;
+        id_start = strchr(new_id_str, '-');
+    }
+
+    if(id_start && *(id_start+1)!='*') {
+        new_id = strtol(id_start + 1, NULL, 10);
+    }
+    else {
+        if(new_millis == curr_millis )
+            new_id = curr_id+1;
+        else if(new_millis > curr_millis)
+            new_id = 0;
+        else
+            assert(0);
+    }
+
+    if(new_millis < 0 || new_id < 0 || (new_millis == 0 && new_id == 0))
+        return ID_00_ERROR;
+    if((new_millis < curr_millis) || new_millis==curr_millis && new_id <= curr_id)
+        return ID_GREATER_THAN_ERR;
+
+    *error = 0;
+    int id_len = snprintf(NULL, 0, "%ld-%ld", new_millis, new_id);
+    char* ret = malloc(id_len+1);
+    snprintf(ret, id_len+1, "%ld-%ld", new_millis, new_id);
+    return ret;
+}
 
 void xadd_command(int connFd, char** commands, int commandLen, struct HMap* hmap){
     if(commandLen<3){
@@ -87,15 +125,6 @@ void xadd_command(int connFd, char** commands, int commandLen, struct HMap* hmap
         }
     }
 
-    char* id_start;
-    long new_millis = strtol(commands[2], &id_start, 10);
-    long new_id = strtol(id_start+1, NULL, 10);
-
-    if(new_millis < 0 || new_id <0 ||(new_millis==0 && new_id==0)){
-        send_helper(connFd, ID_00_ERROR, strlen(ID_00_ERROR));
-        return;
-    }
-
     if(!entry){
         entry = malloc(sizeof *entry);
         entry->key = malloc(strlen (commands[1])+1);
@@ -108,24 +137,18 @@ void xadd_command(int connFd, char** commands, int commandLen, struct HMap* hmap
         entry->len = 0;
         entry->data = NULL;
     }
-    else{
-        //Validate that id is smaller than current id
-        long curr_millis = strtol(entry->data->id, &id_start, 10);
-        long curr_id = strtol(id_start+1, NULL, 10);
 
-        if(strcmp("*",commands[2])!=0){
+    int error;
+    char * id_or_error = validate_and_generate_key(commands[2], entry->data?entry->data->id:"0-0", &error);
 
-           if(new_millis<curr_millis || (new_millis==curr_millis && new_id<=curr_id)){
-                printf("Greater Than Error\n");
-               fflush(stdout);
-                send_helper(connFd, ID_GREATER_THAN_ERR, strlen(ID_GREATER_THAN_ERR));
-                return;
-            }
-        }
+    if(error){
+        send_helper(connFd, id_or_error, (int)strlen(id_or_error));
+        return;
     }
 
     struct StreamData* data = create_stream_data(commands+2, commandLen-2);
 
+    data->id = id_or_error;
     data->prev = entry->data;
     entry->data = data;
     entry->len++;
